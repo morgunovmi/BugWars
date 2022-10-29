5e98a2fba919e9f301b61feaa5d1dd011c325f65
Max <morgunov.mi@phystech.edu>
2022-10-29 22:38:10 +0300

    Shooting is scuffed but test are passing

--- BugWars/Source/Code/Tank.cpp
+++ BugWars/Source/Code/Tank.cpp
@@ -16,7 +16,7 @@ BugBase* Tank::GetBugToShoot() const
 	for (auto obj : g_Game->objects)
 		if (obj->GetRTTI() == Bug::s_RTTI)
 		{
-			auto bug = static_cast<Bug*>(obj);
+			auto* bug = static_cast<Bug*>(obj);
 			return bug;
 		}
 
@@ -25,5 +25,27 @@ BugBase* Tank::GetBugToShoot() const
 
 Point Tank::CalcShootDirection(Point target_pos, Point target_dir, float target_vel, float bullet_vel) const
 {
-	return { 1, 0 };
+	const auto vec_to_target = target_pos - position;
+	const auto a = target_vel * target_vel * target_dir.Length2() - bullet_vel * bullet_vel;
+	const auto b = 2 * target_vel * target_dir.Dot(vec_to_target);
+	const auto c = vec_to_target.Length2();
+
+	const auto p = -b / (2 * a);
+	const auto q = std::sqrtf((b * b) - 4 * a * c) / (2 * a);
+
+	const auto t1 = p - q;
+	const auto t2 = p + q;
+	float t = 0.f;
+
+	if (t1 > t2 && t2 > 0.f)
+	{
+		t = t2;
+	}
+	else
+	{
+		t = t1;
+	}
+
+	const auto bug_final_pos = target_pos + target_dir * target_vel * t;
+	return (bug_final_pos - position).Normalized();
 }
